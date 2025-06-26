bf13617e5a7e51814bc6f8ddd89e7841388a1203
Ha Thai Duong <duongthaiha@gmail.com>
2025-06-26 22:44:25 +0000

    Configure Azure Developer Pipeline

--- airedteaming/ai_redteaming.py
+++ airedteaming/ai_redteaming.py
@@ -89,7 +89,7 @@ async def run_red_team():
             red_team = RedTeam(
                 azure_ai_project=project_endpoint,
                 credential=credential,
-                risk_categories=[RiskCategory.Violence],
+                # risk_categories=[RiskCategory.HateUnfairness],
                 num_objectives=1,
                 output_dir="redteam_outputs/"
             )
@@ -98,7 +98,7 @@ async def run_red_team():
             result = await red_team.scan(
                 target=agent_callback,
                 scan_name="Agent-Scan",
-                attack_strategies=[AttackStrategy.Flip],
+                attack_strategies=[AttackStrategy.DIFFICULT],
             )
             print("Red Team scan complete.")
 
